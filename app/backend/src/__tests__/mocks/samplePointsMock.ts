1a63049990bf21ea89d453e0fb41e79506843d4a
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-01 18:03:47 -0300

    chore: creates samplePoint mock

---
+++ app/backend/src/__tests__/mocks/samplePointsMock.ts
@@ -0,0 +1,10 @@
+import Sample from '../../@types/sample.type';
+
+const samplePoint: Sample = {
+  id: 1,
+  name: 'ponto 1',
+  xCoordinate: 70.54,
+  yCoordinate: -40.24,
+};
+
+export default samplePoint;
