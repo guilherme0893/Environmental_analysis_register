3db68a04fc66ee37132befa15d99049e69ff8f9d
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-01 15:39:13 -0300

    chore: creates app.ts, and refact: refacts index.ts

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,14 +1,8 @@
-import express from 'express';
-require('dotenv').config();
-
-const app = express();
-
-app.use(express.json());
+import 'dotenv/config';
+import app from './app';
 
 const PORT = process.env.PORT;
 
-const server = app.listen(PORT, () => console.log(
-  `Server is running on PORT: ${PORT}`,
+app.listen(PORT, () => console.log(
+  `Server is running on PORT: ${PORT}!`,
 ));
-
-export default server;
