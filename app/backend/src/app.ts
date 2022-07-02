e6b7ba4530da734fa6c42c4aa5da30c4c82fc60f
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-01 23:48:08 -0300

    fix: corrects the missing space in the app.ts

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (_request: Request, response: Response) =>
-  response.send({ status: 'I am alive!'}));
+  response.send({ status: 'I am alive!' }));
 
 export default app;
