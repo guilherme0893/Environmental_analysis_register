fc86f38502275700e2df16151366dd69221e0c61
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-05 19:40:23 -0300

    test: creates form test file

--- app/frontend/src/__tests__/components/form.test.js
+++ app/frontend/src/__tests__/components/form.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Form from '../../components/Header';
+import renderWithRouter from '../../renderWithRouter';
 
 describe('Tests the Form component', () => {
-  beforeEach(() => {
-    render(<Form />);
+  test('It should be rendered with a data-testId of sample-form', () => {
+    renderWithRouter(<Form />);
+    expect(screen.queryByTestId('sample-form')).toBeDefined();
   });
-
-  it('It should be rendered with a data-testId of sampleForm', () => {
-    expect(screen.getByTestId(/sample-form/i)).toBeDefined();
+  test('It should have a div with 03 labels tags', () => {
+    renderWithRouter(<Form />);
+    const labels = expect(screen.queryAllByRole('label'));
+    expect(labels).toBeDefined();
+  });
+  test('It should have a div with 03 input tags', () => {
+    renderWithRouter(<Form />);
+    const labels = expect(screen.queryAllByRole('input'));
+    expect(labels).toBeDefined();
   });
 });
