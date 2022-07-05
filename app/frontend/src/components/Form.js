991f8e2333e7b56400b45dce2dc92db46c69b6b5
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-05 19:50:58 -0300

    chore: updates form component

--- app/frontend/src/components/Form.js
+++ app/frontend/src/components/Form.js
@@ -3,7 +3,17 @@ import React from 'react';
 function Form() {
   return (
     <div>
-      <form data-testid="sample-form" />
+      <form data-testid="sample-form">
+        <label htmlFor="sampleName">
+          <input />
+        </label>
+        <label htmlFor="sampleXCoordinate">
+          <input />
+        </label>
+        <label htmlFor="sampleYCoordinate">
+          <input />
+        </label>
+      </form>
     </div>
   );
 }
