8e7324a1daf572dfcae4300f634092c5720a7d28
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-05 19:51:33 -0300

    chore: updates Main page component adding Form

--- app/frontend/src/pages/Main.js
+++ app/frontend/src/pages/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Form from '../components/Form';
 import Header from '../components/Header';
 
-const Main = () => {
+function Main() {
   return (
     <div>
       <Header />
+      <Form />
     </div>
-  )
+  );
 }
 
 export default Main;
