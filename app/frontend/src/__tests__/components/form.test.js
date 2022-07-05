d361e8fdeeb766fa16cac9031f29fc0f60e66e18
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-05 19:50:05 -0300

    test: updates form.test file

--- app/frontend/src/__tests__/components/form.test.js
+++ app/frontend/src/__tests__/components/form.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import Form from '../../components/Header';
 import renderWithRouter from '../../renderWithRouter';
 
