2dd2b014f56de6133dc42172c68229427858c399
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-05 20:39:02 -0300

    test: updates form tests

--- app/frontend/src/__tests__/components/form.test.js
+++ app/frontend/src/__tests__/components/form.test.js
@@ -18,4 +18,13 @@ describe('Tests the Form component', () => {
     const labels = expect(screen.queryAllByRole('input'));
     expect(labels).toBeDefined();
   });
+  test('It should have a div with 03 texts: name, x coordinate, y coordinate', () => {
+    renderWithRouter(<Form />);
+    const name = screen.getByText(/name/i);
+    const xCoordinate = screen.getByText(/x coordinate/i);
+    const yCoordinate = screen.getByText(/y coordinate/i);
+    expect(name).toBeInTheDocument();
+    expect(xCoordinate).toBeInTheDocument();
+    expect(yCoordinate).toBeInTheDocument();
+  });
 });
