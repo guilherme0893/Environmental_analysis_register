2e1fb966a09ec17b49c96f8b4e2d8431e9597bb9
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-06 01:21:26 -0300

    feat: creates simples get "/"

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express';
+import Samples from './routes/samplePoints';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (_request: Request, response: Response) =>
-  response.send({ status: 'I am alive!' }));
+app.use(Samples);
+
+// app.get('/', (_request: Request, response: Response) =>
+//   response.send({ status: 'I am alive!' }));
 
 export default app;
