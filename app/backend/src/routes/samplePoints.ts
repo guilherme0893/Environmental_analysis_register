4ea6528ba5ee9ba0d4a82ffb8ab26728a1eeffc7
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-07 01:43:43 -0300

    chore: creates samples middlewares and adds to routes

--- app/backend/src/routes/samplePoints.ts
+++ app/backend/src/routes/samplePoints.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express';
 import SampleController from '../controllers/samplePoints';
+import SamplePointsValidation from '../middlewares/samplePoints';
 
 const samples = new SampleController();
 
+const samplesValidation = new SamplePointsValidation();
+
 const routes = Router();
 
-routes.get('/', samples.getAll);
+routes.get('/samples', samples.getAll);
 
-routes.post('/', samples.create);
+routes.post(
+  '/samples',
+  samplesValidation.sampleNameValidation,
+  samplesValidation.xCoordinateValidation,
+  samplesValidation.yCoordinateValidation,
+  samples.create,
+);
 
 export default routes;
