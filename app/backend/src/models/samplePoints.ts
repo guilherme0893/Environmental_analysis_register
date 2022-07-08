007275ee24ad2bd7e60f58a77e10f32a9ef174d6
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-08 02:17:20 -0300

    chore: creates getNyName in sampleModel for verification

--- app/backend/src/models/samplePoints.ts
+++ app/backend/src/models/samplePoints.ts
@@ -9,11 +9,11 @@ class SamplePointModel {
     return samplePoints as ISample[];
   };
 
-  // public getOne = async (id: number): Promise<ISample[]> => {
-  //   const query = `SELECT * FROM ArcadisChallenge.samplePoints WHERE id = ?`;
-  //   const [samplePoint] = await connection.execute(query, [id]);
-  //   return samplePoint as ISample[];
-  // };
+  public getByName = async (name: string): Promise<ISample[]> => {
+    const query = 'SELECT * FROM ArcadisChallenge.samplePoints WHERE name = ?';
+    const [samplePoint] = await connection.execute(query, [name]);
+    return samplePoint as ISample[];
+  };
 
   public create = async (
     name: string,
@@ -21,7 +21,7 @@ class SamplePointModel {
     yCoordinate: number,
   ): Promise<ISample> => {
     const query = `INSERT INTO ArcadisChallenge.samplePoints
-      (name, x_coordinate, y_coordinate) VALUES (?,?,?)`;
+      (name, x_coordinate, y_coordinate) VALUES(?,?,?)`;
     const [sample] = await connection.execute<ResultSetHeader>(
       query,
       [name, xCoordinate, yCoordinate],
