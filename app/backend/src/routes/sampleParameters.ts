72f7ae610360674d4502fcc47bd78800acfaa050
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-08 02:38:32 -0300

    chore: adds the middleware to post /parameters route

--- app/backend/src/routes/sampleParameters.ts
+++ app/backend/src/routes/sampleParameters.ts
@@ -1,12 +1,22 @@
 import { Router } from 'express';
 import SampleParameterController from '../controllers/sampleParameters';
+import ParametersValidation from '../middlewares/sampleParameters';
 
 const parameter = new SampleParameterController();
 
+const parametersValidation = new ParametersValidation();
+
 const routes = Router();
 
-routes.get('/parameters', parameter.getAll);
+routes.post(
+  '/parameters',
+  parametersValidation.samplePointNameValidation,
+  parametersValidation.parameterValidation,
+  parametersValidation.parameterUnityValidation,
+  parametersValidation.parameterValueValidation,
+  parameter.create,
+);
 
-routes.post('/parameters', parameter.create);
+routes.get('/parameters', parameter.getAll);
 
 export default routes;
