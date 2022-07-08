d1ee2d363ac18f4129f7ab30baf526dfdf9aa714
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-08 02:45:54 -0300

    chore: Adds components to Main component

--- app/frontend/src/pages/Main.js
+++ app/frontend/src/pages/Main.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import Form from '../components/Form';
+import FormSelect from '../components/FormSelect';
 import Header from '../components/Header';
-import Table from '../components/Table';
+import Form from '../components/Form';
+import DataTable from '../components/DataTable';
+import TableSelect from '../components/TableSelect';
 
 function Main() {
   return (
     <div>
       <Header />
+      <FormSelect />
       <Form />
-      <Table />
+      <TableSelect />
+      <DataTable />
     </div>
   );
 }
