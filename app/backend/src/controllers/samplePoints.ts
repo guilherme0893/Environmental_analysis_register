24555baa53ae5992fac7b792284142c6c9071009
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-07-08 02:18:12 -0300

    chore: updates the create, checking if the sample exists

--- app/backend/src/controllers/samplePoints.ts
+++ app/backend/src/controllers/samplePoints.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
 import SampleService from '../services/samplePoints';
+import SamplePointModel from '../models/samplePoints';
 
 class SampleController {
   public sampleService = new SampleService();
+
+  public sampleModel = new SamplePointModel();
 
   public getAll = async (_req:Request, res:Response): Promise<Response> => {
     const samples = await this.sampleService.getAll();
@@ -11,7 +14,12 @@ class SampleController {
 
   public create = async (req: Request, res: Response): Promise<Response> => {
     const { name, xCoordinate, yCoordinate } = req.body;
+    const checkSample = await this.sampleModel.getByName(name);
+    if (checkSample.length > 0) {
+      return res.status(409).json({ message: 'Sample already registered' });
+    }
     const sample = await this.sampleService.create(name, xCoordinate, yCoordinate);
+    console.log(sample);
     return res.status(201).json(sample);
   };
 }
