a0f31d40ab56bee6d75de4b6fe48a5564b31b05c
Guilherme <you@example.com>
2023-05-26 21:24:43 -0300

    chore: updates mocks

---
+++ app/backend/src/utils/mocks/pointParameters/pointParametersMock.ts
@@ -0,0 +1,35 @@
+export const pointParametersMock = {
+  id: 1,
+  pointId: 1,
+  parameterId: 1,
+  value: 0.05,
+  createdAt: '2023-05-02T23:55:42.528Z' as unknown as Date,
+  updatedAt: '2023-05-02T23:55:42.528Z' as unknown as Date,
+};
+
+export const pointsParametersMock = [
+  {
+    id: 1,
+    pointId: 1,
+    parameterId: 1,
+    value: 0.05,
+    createdAt: '2023-05-02T23:55:42.528Z' as unknown as Date,
+    updatedAt: '2023-05-02T23:55:42.528Z' as unknown as Date,
+  },
+  {
+    id: 2,
+    pointId: 2,
+    parameterId: 2,
+    value: 0.05,
+    createdAt: '2023-05-02T23:55:42.528Z' as unknown as Date,
+    updatedAt: '2023-05-02T23:55:42.528Z' as unknown as Date,
+  },
+  {
+    id: 3,
+    pointId: 3,
+    parameterId: 2,
+    value: 0.06,
+    createdAt: '2023-05-02T23:55:42.528Z' as unknown as Date,
+    updatedAt: '2023-05-02T23:55:42.528Z' as unknown as Date,
+  },
+];
