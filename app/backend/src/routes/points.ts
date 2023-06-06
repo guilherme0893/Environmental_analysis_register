84408cb6a1e94459db2dd3c47454184227b75a04
Guilherme <you@example.com>
2023-06-05 21:45:04 -0300

    chore: updates controllers, route, and service

--- app/backend/src/routes/points.ts
+++ app/backend/src/routes/points.ts
@@ -6,6 +6,7 @@ const points = new PointsController();
 const routes = Router();
 
 routes.get('/index/:id', points.getOne);
+routes.get('/index/parameter/:parameterId', points.getByParameter);
 routes.get('/index', points.getAll);
 routes.post('/create', points.create);
 routes.delete('/delete/:id', points.delete);
